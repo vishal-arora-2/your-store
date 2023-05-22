44fe374c5f35d5796be366374181272a8125c07e
Aryan Tiwari <aryan.tiwari@globallogic.com>
2023-05-22 15:43:16 +0530

    ,

--- src/test/components/porductPage/Product.test.js
+++ src/test/components/porductPage/Product.test.js
@@ -6,6 +6,24 @@ import store from "../../../store";
 
 
 describe("test of Product Section Page" , () =>{
+      test("Add to Cart Button", () => {
+        const { getByText } = render(
+          <Provider store={store}>
+            <Products  />
+          </Provider>
+        );
+        expect(getByText("Add to Cart")).toBeTruthy();
+      });
+      test("Add to Cart Button On Click", () => {
+        const { getByTestId, getByText } = render(
+          <Provider store={store}>
+            <Products data={data} />
+          </Provider>
+        );
+        fireEvent.click(getByText("Add to Cart"));
+        const quantity = getByTestId("quant");
+        expect(quantity.textContent).toBe("1");
+      });
     test("Category exists", () => {
         const { getByText } = render(
             <Provider store={store}>
